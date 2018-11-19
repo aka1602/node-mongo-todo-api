6dbbaf66824869c52a599bb1b57d18ca191f33fe
akash <akash.greenwork1@gmail.com>
2018-11-19 11:24:44 +0530

    add deleteScript

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -7,9 +7,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
   console.log("Connected to MongoDb Server");
   const db = client.db('TodoApp');
 //deleteMany
-  db.collection('Todos').deleteMany({text: "Eat Lunch"});
+  // db.collection('Todos').deleteMany({"test": "Eat Lunch"}).then((result)=>{
+  //   console.log(result);
+  // });
 //deleteOne
-
+// db.collection('Todos').deleteOne({"text": "Eat Lunch"}).then((result)=>{
+//   console.log(result);
+// });
 //findOneAndDelete
+db.collection('Todos').findOneAndDelete({"completed": false}).then((result)=>{
+  console.log(result);
+});
 //  client.close();
 });
